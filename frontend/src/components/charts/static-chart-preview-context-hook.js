aa66e0971ed4dd01e3ea0ba78cffd0acd12899aa
1iFabi <fabiayala2010@hotmail.com>
2026-09-15 14:56:52 -0300

    chore: charts component

---
+++ frontend/src/components/charts/static-chart-preview-context-hook.js
@@ -0,0 +1,7 @@
+import { createContext, useContext } from "react";
+
+export const StaticChartPreviewContext = createContext(false);
+
+export function useStaticChartPreview() {
+  return useContext(StaticChartPreviewContext);
+}
